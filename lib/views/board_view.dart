c2e151bc48ce3881b5309e4508167c77381faf8f
lucas <lucaslima.94@hotmail.com.br>
2023-05-30 17:12:44 -0300

    add colision landing and new piece

--- lib/views/board_view.dart
+++ lib/views/board_view.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:lima_tetris/piece.dart';
 import 'package:lima_tetris/pixel.dart';
 import 'package:lima_tetris/values.dart';
+
+List<List<Tetromino?>> gameBoard =
+    List.generate(colLength, (i) => List.generate(rowLength, (j) => null));
 
 class BoardView extends StatefulWidget {
   const BoardView({super.key});
@@ -30,9 +34,55 @@ class _BoardViewState extends State<BoardView> {
   void gameLoop(Duration frameRate) {
     Timer.periodic(frameRate, (timer) {
       setState(() {
+        checkLanding();
         currentPiece.movePiece(Direction.down);
       });
     });
+  }
+
+  bool checkCollision(Direction direction) {
+    for (int index = 0; index < currentPiece.position.length; index++) {
+      int row = (currentPiece.position[index] / rowLength).floor();
+      int col = currentPiece.position[index] % rowLength;
+
+      if (direction == Direction.left) {
+        col -= 1;
+      } else if (direction == Direction.right) {
+        col += 1;
+      } else if (direction == Direction.down) {
+        row += 1;
+      }
+      if (col > 0 && row > 0 && gameBoard[row][col] != null) {
+        return true;
+      }
+
+      if (row >= colLength || col < 0 || col >= rowLength) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void checkLanding() {
+    if (checkCollision(Direction.down)) {
+      for (int index = 0; index < currentPiece.position.length; index++) {
+        int row = (currentPiece.position[index] / rowLength).floor();
+        int col = currentPiece.position[index] % rowLength;
+
+        if (row >= 0 && col >= 0) {
+          gameBoard[row][col] = currentPiece.type;
+        }
+      }
+      createNewPiece();
+    }
+  }
+
+  void createNewPiece() {
+    Random rand = Random();
+    Tetromino randomType =
+        Tetromino.values[rand.nextInt(Tetromino.values.length)];
+    currentPiece = Piece(type: randomType);
+    currentPiece.initializePiece();
   }
 
   @override
@@ -45,9 +95,16 @@ class _BoardViewState extends State<BoardView> {
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: rowLength),
         itemBuilder: (context, index) {
+          int row = (index / rowLength).floor();
+          int col = index % rowLength;
           if (currentPiece.position.contains(index)) {
             return Pixel(
               color: Colors.yellow,
+              numbers: index.toString(),
+            );
+          } else if (gameBoard[row][col] != null) {
+            return Pixel(
+              color: Colors.pink,
               numbers: index.toString(),
             );
           } else {
